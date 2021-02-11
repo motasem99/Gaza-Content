515b665bfec40d5441257ab352a63d31b40e98ee
mutasem <motasemimad10@gmail.com>
2021-02-11 17:13:43 +0200

    signin and signup

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,6 +5,8 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
+import styles from './Nav.module.css';
+
 import Languege from './Language';
 import { Link } from 'react-router-dom';
 
@@ -25,27 +27,30 @@ const Nav = ({ direction, setDirection }) => {
     <div className={classes.root}>
       <AppBar position='static'>
         <Toolbar>
-          <Button
-            className={classes.menuButton}
-            component={Link}
-            to='/signin'
-            color='inherit'
-          >
-            Login
-          </Button>
-          <Button
-            className={classes.menuButton}
-            component={Link}
-            to='/signup'
-            color='inherit'
-          >
-            Signup
-          </Button>
-          <Languege
-            className={classes.menuButton}
-            direction={direction}
-            setDirection={setDirection}
-          />
+          <h2>LOGO</h2>
+          <nav className={styles.navbar}>
+            <Button
+              className={classes.menuButton}
+              component={Link}
+              to='/signin'
+              color='inherit'
+            >
+              Login
+            </Button>
+            <Button
+              className={classes.menuButton}
+              component={Link}
+              to='/signup'
+              color='inherit'
+            >
+              Signup
+            </Button>
+            <Languege
+              className={classes.menuButton}
+              direction={direction}
+              setDirection={setDirection}
+            />
+          </nav>
         </Toolbar>
       </AppBar>
     </div>
--- src/pages/signin/Signin.jsx
+++ src/pages/signin/Signin.jsx
@@ -1,7 +1,105 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-const Signin = () => {
-  return <div>Sign in Page</div>;
-};
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  menuButton: {
+    fontSize: '12px',
+    color: '#1976d2',
+  },
+}));
 
-export default Signin;
+export default function SignIn() {
+  const classes = useStyles();
+
+  return (
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            id='email'
+            label='Email Address'
+            name='email'
+            autoComplete='email'
+            autoFocus
+          />
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            name='password'
+            label='Password'
+            type='password'
+            id='password'
+            autoComplete='current-password'
+          />
+          <FormControlLabel
+            control={<Checkbox value='remember' color='primary' />}
+            label='Remember me'
+          />
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='primary'
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Button
+                variant='body2'
+                className={classes.menuButton}
+                component={Link}
+                to='/signin'
+                color='inherit'
+              >
+                Don't have an account? Sign up
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}
