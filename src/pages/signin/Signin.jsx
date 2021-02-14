738009542cf73b0f6aaadd8b8cfd74ee8461a908
mutasem <motasemimad10@gmail.com>
2021-02-14 18:39:58 +0200

    add lang to sign in page form

--- src/pages/signin/Signin.jsx
+++ src/pages/signin/Signin.jsx
@@ -8,7 +8,11 @@ import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import {
+  makeStyles,
+  ThemeProvider,
+  createMuiTheme,
+} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { Link } from 'react-router-dom';
 import LoginLangObject from '../../Languages/Login';
@@ -41,6 +45,10 @@ export default function SignIn() {
   const classes = useStyles();
   const [error, setError] = useState('');
   const Language = localStorage.getItem('dirLang') === 'ltr' ? 'EN' : 'AR';
+  const dirTheme = localStorage.getItem('dirLang');
+  const ThemeDirection = createMuiTheme({ direction: dirTheme });
+
+  console.log(dirTheme);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -54,68 +62,70 @@ export default function SignIn() {
   };
 
   return (
-    <Container component='main' maxWidth='xs'>
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component='h1' variant='h5'>
-          {LoginLangObject.login[Language]}
-        </Typography>
-        <form className={classes.form} noValidate onSubmit={handleSubmit}>
-          <TextField
-            variant='outlined'
-            margin='normal'
-            required
-            fullWidth
-            id='email'
-            label={LoginLangObject.email[Language]}
-            name='email'
-            autoComplete='email'
-            autoFocus
-            error={error === 'email' ? true : false}
-          />
-          <TextField
-            variant='outlined'
-            margin='normal'
-            required
-            fullWidth
-            name='password'
-            label={LoginLangObject.password[Language]}
-            type='password'
-            id='password'
-            autoComplete='current-password'
-            error={error === 'password' ? true : false}
-          />
-          <FormControlLabel
-            control={<Checkbox value='remember' color='primary' />}
-            label={LoginLangObject.remember[Language]}
-          />
-          <Button
-            type='submit'
-            fullWidth
-            variant='contained'
-            color='primary'
-            className={classes.submit}
-          >
-            {LoginLangObject.signin[Language]}
-          </Button>
-          <Grid container>
-            <Grid item>
-              <Button
-                variant='body2'
-                className={classes.menuButton}
-                component={Link}
-                to='/signup'
-                color='inherit'
-              >
-                {LoginLangObject.noAccount[Language]}
-              </Button>
+    <ThemeProvider theme={ThemeDirection}>
+      <Container component='main' maxWidth='xs'>
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component='h1' variant='h5'>
+            {LoginLangObject.login[Language]}
+          </Typography>
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
+            <TextField
+              variant='outlined'
+              margin='normal'
+              required
+              fullWidth
+              id='email'
+              label={LoginLangObject.email[Language]}
+              name='email'
+              autoComplete='email'
+              autoFocus
+              error={error === 'email' ? true : false}
+            />
+            <TextField
+              variant='outlined'
+              margin='normal'
+              required
+              fullWidth
+              name='password'
+              label={LoginLangObject.password[Language]}
+              type='password'
+              id='password'
+              autoComplete='current-password'
+              error={error === 'password' ? true : false}
+            />
+            <FormControlLabel
+              control={<Checkbox value='remember' color='primary' />}
+              label={LoginLangObject.remember[Language]}
+            />
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              color='primary'
+              className={classes.submit}
+            >
+              {LoginLangObject.signin[Language]}
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Button
+                  variant='body2'
+                  className={classes.menuButton}
+                  component={Link}
+                  to='/signup'
+                  color='inherit'
+                >
+                  {LoginLangObject.noAccount[Language]}
+                </Button>
+              </Grid>
             </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
+          </form>
+        </div>
+      </Container>
+    </ThemeProvider>
   );
 }
