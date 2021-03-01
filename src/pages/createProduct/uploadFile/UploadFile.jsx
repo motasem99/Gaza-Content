f09053d31156073558bc2823ccaa25d3fd3bc937
mutasem <motasemimad10@gmail.com>
2021-03-01 18:52:00 +0200

    upload form

--- src/pages/createProduct/uploadFile/UploadFile.jsx
+++ src/pages/createProduct/uploadFile/UploadFile.jsx
@@ -1,5 +1,149 @@
+import { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { Form } from 'react-final-form';
+import { FormLabel } from '@material-ui/core';
+import { Paper, Grid, CssBaseline } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+    display: 'flex',
+    alignItems: 'center',
+  },
+  input: {
+    display: 'none',
+  },
+  mainForm: {
+    padding: 16,
+    margin: 'auto',
+    maxWidth: 500,
+    border: 'none',
+  },
+  paper: {
+    padding: 16,
+    boxShadow: 'none',
+  },
+  gridButton: {
+    marginTop: 16,
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  submitButton: {
+    width: '70%',
+  },
+  formControl: {
+    width: '100%',
+  },
+  img: {
+    width: '200px',
+    height: '180px',
+  },
+  divImg: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+  },
+}));
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  console.log('hiii');
+};
+
+const fileToDataUri = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      resolve(event.target.result);
+    };
+    reader.readAsDataURL(file);
+  });
+
 const UploadFile = () => {
-  return <h1>iam second</h1>;
+  const classes = useStyles();
+  //   const [file, setFile] = useState(null);
+  const [file, setFile] = useState('');
+
+  const onChange = (file) => {
+    if (!file) {
+      setFile('');
+      return;
+    }
+
+    fileToDataUri(file).then((file) => {
+      setFile(file);
+    });
+  };
+
+  return (
+    <div className={classes.mainForm}>
+      <CssBaseline />
+      <Form
+        onSubmit={onSubmit}
+        initialValues={{ employed: true, stooge: 'larry' }}
+        render={() => (
+          <form onSubmit={onSubmit} noValidate>
+            <Paper className={classes.paper}>
+              <Grid container alignItems='flex-start' spacing={2}>
+                <Grid item xs={12} className={classes.root}>
+                  <FormLabel component='legend' className={classes.formControl}>
+                    Upload The Product Preview:{' '}
+                  </FormLabel>
+                  <input
+                    accept='image/*'
+                    className={classes.input}
+                    id='icon-button-file'
+                    type='file'
+                    onChange={(event) =>
+                      onChange(event.target.files[0] || null)
+                    }
+                  />
+                  <label htmlFor='icon-button-file'>
+                    <IconButton
+                      color='primary'
+                      aria-label='upload picture'
+                      component='span'
+                    >
+                      <PhotoCamera />
+                    </IconButton>
+                  </label>
+                </Grid>
+                <div className={classes.divImg}>
+                  {file && <img src={file} alt={''} className={classes.img} />}
+                </div>
+                <Grid item xs={12} className={classes.root}>
+                  <FormLabel component='legend' className={classes.formControl}>
+                    Upload The Product :{' '}
+                  </FormLabel>
+                  <input
+                    accept='image/*'
+                    className={classes.input}
+                    id='icon-button-file'
+                    type='file'
+                  />
+                  <label htmlFor='icon-button-file'>
+                    <IconButton
+                      color='primary'
+                      aria-label='upload picture'
+                      component='span'
+                    >
+                      <PhotoCamera />
+                    </IconButton>
+                  </label>
+                </Grid>
+              </Grid>
+            </Paper>
+          </form>
+        )}
+      />
+    </div>
+  );
 };
 
 export default UploadFile;
