3051a24a6c7712b9f8f04f473df66d1071c6e2a6
mutasem <motasemimad10@gmail.com>
2021-03-03 19:45:43 +0200

    clear code

--- src/pages/createProduct/preview/Preview.jsx
+++ src/pages/createProduct/preview/Preview.jsx
@@ -6,6 +6,8 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
 import EuroSymbolIcon from '@material-ui/icons/EuroSymbol';
+import { Fragment } from 'react';
+import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,46 +22,106 @@ const useStyles = makeStyles((theme) => ({
   dividerInset: {
     margin: `5px 0 0 ${theme.spacing(9)}px`,
   },
+  infoFile: {
+    width: '50%',
+    backgroundColor: 'white',
+    height: '500px',
+    margin: 'auto',
+  },
+  divSecondFile: {
+    marginTop: '40px',
+    border: '1px dashed #ccc',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    paddingBottom: '15px',
+    paddingTop: '15px',
+    alignItems: 'center',
+  },
+  spanSecondFile: {
+    marginLeft: '5px',
+  },
+  img: {
+    width: '100%',
+    height: '350px',
+    borderRadius: '8px',
+    '&:hover': {
+      filter: 'blur(1.8px)',
+    },
+  },
+  divImg: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+  },
 }));
 
-const Preview = ({ form }) => {
+const Preview = ({ form, secondFile, file }) => {
   console.log(form.currency);
   const classes = useStyles();
 
   return (
-    <List className={classes.root}>
-      <ListItem>
-        <ListItemText primary='Name Product' secondary={form.name} />
-      </ListItem>
-      <Divider component='li' />
-      <ListItem>
-        <ListItemText
-          primary='Price'
-          secondary={
-            <div
-              style={{
-                display: 'flex',
-              }}
-            >
-              <p style={{ marginRight: '3px', fontSize: '20px' }}>
-                {form.price}
-              </p>
-              {form.currency === 'dollar' ? (
-                <p>
-                  <AttachMoneyIcon />
+    <Fragment>
+      <List className={classes.root}>
+        <ListItem>
+          <ListItemText
+            primary='Name Product'
+            secondary={
+              <div>
+                <p style={{ fontSize: '20px' }}>{form.name}</p>
+              </div>
+            }
+          />
+        </ListItem>
+        <Divider component='li' />
+        <ListItem>
+          <ListItemText
+            primary='Price'
+            secondary={
+              <div
+                style={{
+                  display: 'flex',
+                }}
+              >
+                <p style={{ marginRight: '3px', fontSize: '20px' }}>
+                  {form.price}
                 </p>
-              ) : (
-                <EuroSymbolIcon />
-              )}
-            </div>
-          }
-        />
-      </ListItem>
-      <Divider component='li' />
-      <ListItem>
-        <ListItemText primary='Description' secondary={form.description} />
-      </ListItem>
-    </List>
+                {form.currency === 'dollar' ? (
+                  <p>
+                    <AttachMoneyIcon />
+                  </p>
+                ) : (
+                  <EuroSymbolIcon />
+                )}
+              </div>
+            }
+          />
+        </ListItem>
+        <Divider component='li' />
+        <ListItem>
+          <ListItemText
+            primary='Description'
+            secondary={
+              <div>
+                <p style={{ fontSize: '20px' }}>{form.description}</p>
+              </div>
+            }
+          />
+        </ListItem>
+      </List>
+      <div className={classes.infoFile}>
+        {file && (
+          <div className={classes.divImg}>
+            <img src={file} alt={''} className={classes.img} />
+          </div>
+        )}
+        <div className={classes.divSecondFile}>
+          <span>Your Product Name: </span>
+          <InsertDriveFileIcon color='primary' fontSize='large' />
+          <span className={classes.spanSecondFile}>{secondFile.name}</span>
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
