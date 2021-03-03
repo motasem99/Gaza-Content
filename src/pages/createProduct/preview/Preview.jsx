30e8e0744151bc4fc59c2908ba387ec4e299431c
mutasem <motasemimad10@gmail.com>
2021-03-03 18:21:16 +0200

    editing on Preview file

--- src/pages/createProduct/preview/Preview.jsx
+++ src/pages/createProduct/preview/Preview.jsx
@@ -1,24 +1,46 @@
-import PreviewFile from '../previewFile/PreviewFile';
-import UploadFile from '../uploadFile/UploadFile';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
 
-const Preview = ({
-  form,
-  setForm,
-  file,
-  setFile,
-  secondFile,
-  setSecondFile,
-}) => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+    margin: 'auto',
+  },
+  dividerFullWidth: {
+    margin: `5px 0 0 ${theme.spacing(2)}px`,
+  },
+  dividerInset: {
+    margin: `5px 0 0 ${theme.spacing(9)}px`,
+  },
+}));
+
+const Preview = ({ form }) => {
+  console.log(form);
+  const classes = useStyles();
+
   return (
-    <div>
-      <PreviewFile form={form} setForm={setForm} />;
-      <UploadFile
-        file={file}
-        setFile={setFile}
-        secondFile={secondFile}
-        setSecondFile={setSecondFile}
-      />
-    </div>
+    <List className={classes.root}>
+      <ListItem>
+        <ListItemText primary='Name Product' secondary={form.name} />
+      </ListItem>
+      <Divider component='li' />
+      <ListItem>
+        <ListItemText
+          primary='Price'
+          secondary={`${form.price} ${form.currency}`}
+        />
+      </ListItem>
+      <Divider component='li' />
+      <ListItem>
+        <ListItemText primary='Description' secondary={form.description} />
+      </ListItem>
+    </List>
   );
 };
 
