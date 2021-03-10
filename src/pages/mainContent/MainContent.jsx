975818cfccafdd8363537913a0fbb9dfa4558062
mutasem <motasemimad10@gmail.com>
2021-03-10 17:16:19 +0200

    clear code

--- src/pages/mainContent/MainContent.jsx
+++ src/pages/mainContent/MainContent.jsx
@@ -22,7 +22,6 @@ import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
 import Slider from '@material-ui/core/Slider';
 
 // PhotoCard
