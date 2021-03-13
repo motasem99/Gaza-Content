02f8572899aaa8ba743471b5dbee666a40aef6e8
mutasem <motasemimad10@gmail.com>
2021-03-13 17:51:32 +0200

    add shadow to sidebar and styling sidebareHeader

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
 import { InputBase } from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
 
 const useStyles = makeStyles((theme) => ({
   main: {
@@ -12,18 +13,21 @@ const useStyles = makeStyles((theme) => ({
   sidebar: {
     border: '1px solid #c4c4c4',
     height: '80vh',
-    width: '25%',
+    width: '28%',
+    boxShadow: '0 5px 20px rgb(0 0 0 / 17%)',
+    borderRadius: '4px',
   },
   chat: {
     width: '70%',
   },
   sidebarHeader: {
-    height: '70px',
+    height: '40px',
     borderBottom: '0.5px solid #c4c4c4',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-around',
     backgroundColor: '#f4f4f4',
+    padding: '10px 29px 10px 10px',
   },
   sidebarTitle: {
     color: '#05728f',
@@ -44,6 +48,37 @@ const useStyles = makeStyles((theme) => ({
     marginRight: '5px',
     marginLeft: '10px',
   },
+  userChat: {
+    display: 'flex',
+    alignItems: 'center',
+    height: '90px',
+    borderBottom: '1px solid #cdcdcd',
+    backgroundColor: '#ebebeb',
+  },
+  avatarUser: {
+    marginRight: '20px',
+    marginLeft: '20px',
+  },
+  infoChat: {
+    width: '100%',
+  },
+  userName: {
+    fontSize: '15px',
+    color: '#464646',
+    margin: '0 0 8px 0',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  infoUserChat: {
+    fontSize: '14px',
+    color: '#989898',
+    margin: 'auto',
+  },
+  spanDate: {
+    fontSize: '13px',
+    marginRight: '20px',
+    marginLeft: '20px',
+  },
 }));
 
 function Chat() {
@@ -61,6 +96,19 @@ function Chat() {
               placeholder='search...'
               inputProps={{ 'aria-label': 'search' }}
             />
+          </div>
+        </div>
+        <div className={classes.userChat}>
+          <Avatar
+            className={classes.avatarUser}
+            alt='Remy Sharp'
+            src='/static/images/avatar/1.jpg'
+          />
+          <div className={classes.infoChat}>
+            <h5 className={classes.userName}>
+              User Name <span className={classes.spanDate}>Date</span>
+            </h5>
+            <p className={classes.infoUserChat}>click to see the chat...</p>
           </div>
         </div>
       </div>
