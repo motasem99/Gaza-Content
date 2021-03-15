0bd9caa5f4f3ac0194acd45e7660748921d0757d
mutasem <motasemimad10@gmail.com>
2021-03-15 12:54:54 +0200

    add signin style in file

--- src/pages/signin/Signin.jsx
+++ src/pages/signin/Signin.jsx
@@ -8,40 +8,15 @@ import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import {
-  makeStyles,
-  ThemeProvider,
-  createMuiTheme,
-} from '@material-ui/core/styles';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 // Router
 import { Link } from 'react-router-dom';
 // Language
 import LoginLangObject from '../../Languages/Login';
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  menuButton: {
-    fontSize: '12px',
-    color: '#1976d2',
-  },
-}));
+// Style
+import useStyles from './SigninStyle.js';
 
 export default function SignIn() {
   const classes = useStyles();
